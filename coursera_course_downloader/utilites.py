1adc6fd06c45d602a00f0d923592c8dbeb1cb92f
Ivan Chen <ujhuyz0110@gmail.com>
2018-02-23 15:12:18 -0800

    add new task

---
+++ coursera_course_downloader/utilites.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+
+
+def login(login_page_url, email, username):
+    """
+    :param login_page_url: for Coursera: https://www.coursera.org/?authMode=login
+    :param email:
+    :param username:
+    :return:
+    """
+    pass
+
+
+def get_lesson_url(weekly_syllabus_url):
+    pass
