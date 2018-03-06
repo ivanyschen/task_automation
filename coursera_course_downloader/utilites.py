7457e8cd5334bc7f24cdccbd69e55bcc2d07e6fe
Ivan C <ivanyschen@berkeley.edu>
2018-03-05 22:31:46 -0800

    hide password

--- coursera_course_downloader/utilites.py
+++ coursera_course_downloader/utilites.py
@@ -3,7 +3,7 @@ from page import LoginPage, CourseHomePage
 
 def login(driver,  page_url):
     login_page = LoginPage(driver, 'https://www.coursera.org/?authMode=login')
-    login_page.fill_up_email('ujhuyz0110@gmail.com')
-    login_page.fill_up_password('Yushuo1992')
+    login_page.fill_up_email('youremail@gmail.com')
+    login_page.fill_up_password('yourpassword')
     login_page.click_login()
     return
